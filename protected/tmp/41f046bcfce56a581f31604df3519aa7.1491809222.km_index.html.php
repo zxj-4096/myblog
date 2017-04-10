7a4fed572261d5995d02b806b23f9103d7e3b05d
381601286@qq.com <381601286@qq.com>
2017-04-10 15:33:15 +0800

    update modify

--- protected/tmp/41f046bcfce56a581f31604df3519aa7.1491809222.km_index.html.php
+++ protected/tmp/41f046bcfce56a581f31604df3519aa7.1491809222.km_index.html.php
@@ -14,7 +14,10 @@
 		</tr>
 		<?php $_foreach_v_counter = 0; $_foreach_v_total = count($findall);?><?php foreach( $findall as $k => $v ) : ?><?php $_foreach_v_index = $_foreach_v_counter;$_foreach_v_iteration = $_foreach_v_counter + 1;$_foreach_v_first = ($_foreach_v_counter == 0);$_foreach_v_last = ($_foreach_v_counter == $_foreach_v_total);$_foreach_v_counter++;?>
 		<tr>
-			<td><a href="<?php echo url(array('m'=>'kmblog', 'c'=>'km', 'a'=>'find', 'id'=>$v["article_id"], ));?>"><?php echo $v["article_name"];; ?></a></td>
+			<td>
+				<a href="<?php echo url(array('m'=>'kmblog', 'c'=>'km', 'a'=>'find', 'id'=>$v["article_id"], ));?>"><?php echo $v["article_name"]; ?></a><?php echo date("Y-m-d",$v["article_time"]);?>
+				<a href="<?php echo url(array('m'=>'kmblog', 'c'=>'km', 'a'=>'update', 'id'=>$v["article_id"], ));?>">修改</a>
+			</td>
 		</tr>
 		<?php endforeach; ?>
 	</table>
